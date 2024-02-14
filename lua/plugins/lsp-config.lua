78a7524b9fd4f1656ebdecd018f7bfd0b8bad898
Anthony Longo <ferrari13@me.com>
2024-02-14 05:48:59 -0500

    Add Harpoon

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,7 @@ return {
       lspconfig.rust_analyzer.setup({
         capabilities = capabilities
       })
+      lspconfig.pyright.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
